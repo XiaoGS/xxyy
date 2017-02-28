0323189edfc68932c69f68be4fb5e4be8417eaa6
奥特曼 <aoteman7788@126.com>
2017-02-28 19:06:27 +0800

    update 002

---
+++ src/com/atguigu/bean/NewATM.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+public class NewATM {
+
+	public static void main(String[] args) {
+		System.out.println("NNNNNNNNNNNNNNNNNNNNN");
+		System.out.println("NNNNNNNNNNNNNNNNNNNNN");
+		System.out.println("NNNNNNNNNNNNNNNNNNNNN");
+	}
+
+}
